7db735999ccf1773caaa89947097a4b2026bf5d6
Alexandr <114095847+Alotiel@users.noreply.github.com>
2022-11-11 20:28:45 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function N() {
+  const inpObj = document.getElementById("id1");
+  if (!inpObj.checkValidity()) {
+    document.getElementById("message").innerHTML = inpObj.validationMessage;
+  } else {
+    document.getElementById("message").innerHTML = "Input OK";
+  }
+}
